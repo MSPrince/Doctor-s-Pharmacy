aacee252fab9c333fd6ad9d16001375d91bbc283
M S Prince <135182786+MSPrince@users.noreply.github.com>
2023-10-01 21:01:24 +0530

    banner Section add kiye pr sahi hua nahi hai abhi

---
+++ js/main.js
@@ -0,0 +1,22 @@
+let bannerList = [
+    {
+        imgUrl: "https://cms-contents.pharmeasy.in/banner/93051e0bc77-MEGA20_App.jpg",
+    },
+    {
+        imgUrl: "https://cms-contents.pharmeasy.in/banner/dc5281c821e-GRAND1000_App.jpg",
+    },
+    {
+        imgUrl: "https://cms-contents.pharmeasy.in/banner/86df3a0dc37-HP.jpg",
+    },
+];
+
+let bannerContainer = document.querySelector(".banner");
+createOffersList(bannerList, bannerContainer)
+
+function createOffersList(list, container){
+    list.foreach(function(el){
+        let img = document.createElement("img");
+        img.setAttribute("src", el.imgUrl);
+        container.append(img);
+    })
+}

2c81a88e83ce86fc419f9f36074e7000798f1bd8
M S Prince <135182786+MSPrince@users.noreply.github.com>
2023-10-02 11:28:43 +0530

    Add trending near you section

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ function createOfferList(list, container) {
 
 
 
-// ---------------------------------------
+// ---------------------------------------new Launches Start----------------------------------->>
 let newLaunchesList = [
     {
         name: "Kotex Overnight Period Panties - Medium / Large Size, Pack Of 2 Panties",
@@ -79,6 +79,7 @@ let newLaunchesList = [
     },
 ];
 
+
 let newLanchesContainer = document.querySelector(".new-launches > .product-cards");
 createProductList(newLaunchesList, newLanchesContainer); // Corrected function name
 
@@ -103,5 +104,58 @@ function createProductList(list, container) {
         container.append(div);
     });
 }
+// --/ ---------------------------------------new Launches End----------------------------------->>
 
-// ---------------------------------------
+
+
+
+// --------------------------------------trending near you Start---------------------------------------->>>
+
+let trendingProdList = [
+    {
+        name: "Supradyn Daily Multivitamin Tablets 15s",
+        imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/T70695/supradyn-daily-multivitamin-tablets-15s-1-1641793954.jpg?dim=320x320&dpr=1&q=100",
+        mrp: 54,
+        discount: 5,
+    },
+    {
+        name: "Supradyn Daily Multivitamin Tablets 15s",
+        imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/T70695/supradyn-daily-multivitamin-tablets-15s-1-1641793954.jpg?dim=320x320&dpr=1&q=100",
+        mrp: 54,
+        discount: 5,
+    },
+    {
+        name: "Everherb Karela Jamun Juice - Helps Maintains Healthy Sugar Levels -Helps In Weight Management - 1l",
+        imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/L79986/everherb-karela-jamun-juice-helps-maintains-healthy-sugar-levels-helps-in-weight-management-1l-2-1654234653.jpg?dim=320x320&dpr=1&q=100",
+        mrp: 399,
+        discount: 48,
+    },
+    {
+        name: "Dr Morepen Gluco One Bg 03 Glucometer Test Strips Box Of 50",
+        imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/I05582/dr-morepen-gluco-one-bg-03-glucometer-test-strips-box-of-50-1-1654168216.jpg?dim=1024x0",
+        mrp: 840,
+        discount: 24,
+    },
+    {
+        name: "Dettol Antiseptic Liquid Bottle Of 550 Ml",
+        imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/I40695/dettol-antiseptic-liquid-bottle-of-550-ml-2-1656923531.jpg?dim=1024x0",
+        mrp: 194.6,
+        discount: 7,
+    },
+    {
+        name: "Liveasy Essentials Copper Bottle - Ayurvedic Health Benefits - Leak Proof Cap - 950ml",
+        imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/W00450/liveasy-essentials-copper-bottle-ayurvedic-health-benefits-leak-proof-cap-950ml-1-1641793079.jpg?dim=1024x0",
+        mrp: 1100,
+        discount: 30,
+    },
+    {
+        name: "Vicks Vaporub 25ml, Relief From Cold, Cough, Headache And Body Pain",
+        imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/181135/vicks-vaporub-25ml-relief-from-cold-cough-headache-and-body-pain-2-1654078746.jpg?dim=1024x0",
+        mrp: 85,
+        discount: 11,
+    },
+];
+
+let trendingContainer = document.querySelector(".trending > .product-cards");
+createProductList(trendingProdList, trendingContainer);
+// --------------------------------------trending near you End---------------------------------------->>>

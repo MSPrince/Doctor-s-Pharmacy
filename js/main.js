d7af2e12fd6419c3d1fbcc22586ea5cdf4498fb2
M S Prince <135182786+MSPrince@users.noreply.github.com>
2023-10-05 14:03:47 +0530

    add Wellness Essential Of The Week Section

--- js/main.js
+++ js/main.js
@@ -279,3 +279,49 @@ function createOfferList(list, container) {
 }
 
 // -----------------------------------------------------------Payment Offers Section End--------------------------------------------------------------------->>>
+
+
+/* < !------------------------------------------------------------welnes essential start----------------------------------------------------------> */
+let wellnessEssentialsList = [
+    {
+        name: "Evion 400mg Strip of 10 Capsules",
+        imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/071710/evion-400mg-strip-of-10-capsules-2-1656662057.jpg?dim=1024x0",
+        mrp: 35,
+        discount: 10,
+    },
+
+    {
+        name: "Neurobin Forte Tablet 30's",
+        imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/I09432/neurobion-forte-tablet-30s-2-1656661904.jpg?dim=1024x0",
+        mrp: 34.7,
+        discount: 10,
+    },
+    {
+        name: "Gas O Fast Active Jeera Axtacid Sachet Of 5 G",
+        imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/I34550/gas-o-fast-active-jeera-antacid-sachet-of-5-g-2-1654166983.jpg?dim=1024x0",
+        mrp: 10,
+        discount: 15,
+    },
+    {
+        name: "Pharmeasy Multivitamin Multimineral - Immunity Booster - Complete Nutrition - Bottle Of 60",
+        imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/T60720/pharmeasy-multivitamin-multimineral-immunity-booster-complete-nutrition-bottle-of-60-2-1658316798.jpg?dim=320x320&dpr=1&q=100",
+        mrp: 54,
+        discount: 15,
+    },
+    {
+        name: "Electral Powder 21.80gm",
+        imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/065285/electral-powder-2180gm-2-1653986181.jpg?dim=1024x0",
+        mrp: 21.05,
+        discount: 15,
+    },
+    {
+        name: "Supradyn Daily Multivitamin Tablets 15s",
+        imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/T70695/supradyn-daily-multivitamin-tablets-15s-1-1641793954.jpg?dim=1024x0",
+        mrp: 54,
+        discount: 5,
+    },
+];
+
+let wellnessEssentialsContainer = document.querySelector(".wellness-essentials > .product-cards2");
+createProductList(wellnessEssentialsList, wellnessEssentialsContainer);
+/* < !------------------------------------------------------------welnes essential end----------------------------------------------------------> */
